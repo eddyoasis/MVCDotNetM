14a532379538c87a22ab485a81dfd6af1683617d
eddyoasis <eddywangoasis@gmail.com>
2025-07-18 10:54:10 +0800

    logFilter: mask for sensitive property

--- Filters/LogFilter.cs
+++ Filters/LogFilter.cs
@@ -1,24 +1,45 @@
 ﻿using Microsoft.AspNetCore.Mvc.Filters;
 using Newtonsoft.Json;
 using Serilog;
+using System.Text;
 
 namespace MVCWebApp.Filters
 {
     public class LogFilter : ActionFilterAttribute
     {
+        private static readonly string[] SensitiveFields = ["Password"];
+
         public override void OnActionExecuting(ActionExecutingContext context)
         {
-            var requests = string.Empty;
-
-            context.ActionArguments.ToList().ForEach(x =>
-            {
-                requests += JsonConvert.SerializeObject(x.Value, Formatting.Indented);
-            });
-
             var controller = context.RouteData.Values["controller"];
             var action = context.RouteData.Values["action"];
 
-            Log.Information(@$"➡️ START: {controller}.{action} req: {requests}");
+            var requests = new StringBuilder();
+
+            foreach (var arg in context.ActionArguments)
+            {
+                var obj = arg.Value;
+                if (obj == null)
+                    continue;
+
+                var json = JsonConvert.SerializeObject(obj);
+                var clone = JsonConvert.DeserializeObject(json, obj.GetType());
+
+                var type = clone.GetType();
+
+                foreach (var field in SensitiveFields)
+                {
+                    var prop = type.GetProperty(field);
+                    if (prop != null && prop.CanWrite)
+                    {
+                        prop.SetValue(clone, "***MASKED***");
+                    }
+                }
+
+                requests.AppendLine(JsonConvert.SerializeObject(clone, Formatting.Indented));
+            }
+
+            Log.Information("➡️ START: {Controller}.{Action} req: {Request}", controller, action, requests.ToString());
         }
 
         public override void OnActionExecuted(ActionExecutedContext context)
